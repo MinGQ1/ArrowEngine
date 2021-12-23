2ea72f1e53ce47e6abb2f59469d14cd4f1c101cf
Zheng <867673046@qq.com>
2021-12-23 23:31:36 +0800

    modified log

--- ArrowEngine/src/ArrowEngine/EntryPoint.h
+++ ArrowEngine/src/ArrowEngine/EntryPoint.h
@@ -7,6 +7,7 @@ extern ArrowEngine::Application* ArrowEngine::CreateApplication();
 int main(int argc, char** argv)
 {
 	ArrowEngine::Log::Init();
+	AE_CORE_WARN("Hello Log!");
 	auto app = ArrowEngine::CreateApplication();
 	app->Run();
 	delete app;
